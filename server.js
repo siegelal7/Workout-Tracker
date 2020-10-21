cdab01cbbe1945b6cba7dc0a7ff496a1ab2ff1b0
Andrew Siegel <siegelal7@gmail.com>
2020-10-21 09:48:32 -0400

    adding seed data

--- server.js
+++ server.js
@@ -4,31 +4,31 @@ const logger = require("morgan");
 const PORT = process.env.PORT || 8080;
 const app = express();
 const db = require("./models");
-// const seed = require("./seeders/seed");
+const seed = require("./seeders/seed");
 // const workoutController = require("./controllers/workoutController");
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/architecture",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://localhost/architecture",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//   }
+// );
 
-const connection = mongoose.connection;
+// const connection = mongoose.connection;
 
-connection.on("connected", () => {
-  console.log("Mongoose successfully connected.");
-});
+// connection.on("connected", () => {
+//   console.log("Mongoose successfully connected.");
+// });
 
-connection.on("error", (err) => {
-  console.log("Mongoose connection error: ", err);
-});
+// connection.on("error", (err) => {
+//   console.log("Mongoose connection error: ", err);
+// });
 
 // app.get("/", (req, res) => {
 //   res.render("./public/index.html");
