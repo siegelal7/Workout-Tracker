7ca626e00fea229332e6c77f1088f4158f23f283
Andrew Siegel <siegelal7@gmail.com>
2020-10-21 20:45:06 -0400

    got up and running i think

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
+// const path = require("path");
 
 const db = require("../models");
 
@@ -36,9 +36,24 @@ router.get("/api/exercises/", (req, res) => {
 });
 
 router.put("/api/workouts/:id", (req, res) => {
-  db.Workout.create(req.body)
+  db.Workout.findByIdAndUpdate(
+    req.params.id,
+    { $push: { exercises: req.body } },
+    { new: true }
+  )
     .then((newExercise) => {
       res.json(newExercise);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .limit(7)
+    .then((foundWorkout) => {
+      res.json(foundWorkout);
     })
     .catch((err) => {
       res.json(err);
--- models/Workout.js
+++ models/Workout.js
@@ -11,26 +11,54 @@ const Schema = mongoose.Schema;
 //   sets: Number,
 // });
 
-const workoutSchema = new mongoose.Schema({
-  day: {
-    type: Date,
-    default: Date.now,
-  },
-  exercises: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Exercise",
+const workoutSchema = new mongoose.Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now,
     },
-    // {
-    //   type: String,
-    //   name: String,
-    //   duration: Number,
-    //   weight: Number,
-    //   reps: Number,
-    //   sets: Number,
-    //   distance: Number,
-    // },
-  ],
+    exercises: [
+      // {
+      //   type: Schema.Types.ObjectId,
+      //   ref: "Exercise",
+      // },
+      {
+        type: {
+          type: String,
+        },
+        name: {
+          type: String,
+          required: "enter a name",
+        },
+        duration: {
+          type: Number,
+        },
+        weight: {
+          type: Number,
+        },
+        reps: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+        distance: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+workoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
